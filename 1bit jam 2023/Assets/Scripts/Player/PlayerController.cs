6b9f6483c7e0444197ca38b59faf54b10c1eab17
Pinho13 <rafaelpinhotsilva@gmail.com>
2023-08-13 23:12:38 +0100

    Shotgun and pistol with anims and shooting working

--- 1bit jam 2023/Assets/Scripts/Player/PlayerController.cs
+++ 1bit jam 2023/Assets/Scripts/Player/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
+    Animator anim;
     [SerializeField] float currentSpeed;
 
     [Header("Walking")]
@@ -15,11 +16,13 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         Movement();
+        faceMouse();
     }
 
     void Movement()
@@ -32,5 +35,21 @@ public class PlayerController : MonoBehaviour
         {
             rb.AddForce((Vector2.up * verticalInput + Vector2.right * horizontalInput).normalized * speedGain * Time.deltaTime * 1000);
         }
+
+        if(verticalInput != 0 || horizontalInput != 0)
+        {
+            anim.SetBool("Walking", true);
+        } else
+        {
+            anim.SetBool("Walking", false);
+        }
+    }
+
+    void faceMouse()
+    {
+        Vector2 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = rotation;
     }
 }
---
+++ 1bit jam 2023/Assets/Scripts/Player/WeaponsManagement.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public enum WeaponType {pistol, shotgun, smg}
+public class WeaponsManagement : MonoBehaviour
+{
+
+    [SerializeField] WeaponType weaponType;
+
+    [Header("AllWeaponStats")]
+    public WeaponStats[] weaponStats;
+
+
+    [Header("CurrentWeapon")]
+    public WeaponStats currentWeaponStats;
+
+    
+
+    [Header("Energy")]
+    [SerializeField] float MaxEnergy;
+    [SerializeField] float currentEnergy;
+
+
+    [Header("References")]
+    Animator gunAnim;
+    float timeToFire;
+    [SerializeField] Rigidbody2D rb;
+
+    void Start()
+    {
+        gunAnim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        WeaponChosen();
+        ShootDelay();
+    }
+    void WeaponChosen()
+    {
+        foreach(WeaponStats stat in weaponStats)
+        {
+            if(weaponType == stat.weaponType)
+            {
+                currentWeaponStats = stat;
+            }
+        }
+    }
+
+    void ShootDelay()
+    {
+        
+        if(currentWeaponStats.fireRate == 0)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                gunAnim.SetBool("Shooting", true);
+            }
+        }
+        else
+        {
+            if(Input.GetMouseButton(0) && Time.time > timeToFire)
+            {
+                timeToFire = Time.time + 1/currentWeaponStats.fireRate;
+                gunAnim.SetBool("Shooting", true);
+            }else if(!Input.GetMouseButton(0))
+            {
+                gunAnim.SetBool("Shooting", false);
+            }
+        }
+    }
+
+    void shoot(int n)
+    {
+        for(int i = 0; i < currentWeaponStats.bulletPerShot; i++)
+        {
+            float currentSpread = Random.Range(-currentWeaponStats.spread, currentWeaponStats.spread);
+            GameObject bullet = Instantiate(currentWeaponStats.bullet, currentWeaponStats.firepoints[n].position, Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z + currentSpread));
+            bullet.GetComponent<Rigidbody2D>().AddForce(bullet.transform.right * currentWeaponStats.bulletForce, ForceMode2D.Impulse);
+        }
+        rb.AddForce(-currentWeaponStats.firepoints[n].right * currentWeaponStats.recoil, ForceMode2D.Impulse);
+    }
+
+    void changeWeapons()
+    {
+        if(!gunAnim.GetCurrentAnimatorStateInfo(0).IsName(weaponType.ToString()))
+        {
+            gunAnim.Play(weaponType.ToString());
+        }
+    }
+    void stopShooting()
+    {
+        gunAnim.SetBool("Shooting", false);
+    }
+
+}
+
+[System.Serializable]
+public class WeaponStats
+{
+    public WeaponType weaponType;
+    public float bulletForce;
+    public float fireRate;
+    public float spread;
+    public float recoil;
+    public int bulletPerShot;
+    public float Damage;
+    public float EnergyConsumed;
+    public GameObject bullet;
+
+    public Transform[] firepoints;
+}
